2ba479a58a00bf5637957c255e98cfa3abedb4bb
xDouglas90 <odouglas.dev@gmail.com>
2023-03-15 14:11:59 -0300

    feat(module-1): implements Reverse functionality :sparkles:

--- module01/reverse.go
+++ module01/reverse.go
@@ -3,9 +3,12 @@ package module01
 // Reverse will return the provided word in reverse
 // order. Eg:
 //
-//   Reverse("cat") => "tac"
-//   Reverse("alphabet") => "tebahpla"
-//
-func Reverse(word string) string {
-	return ""
+//	Reverse("cat") => "tac"
+//	Reverse("alphabet") => "tebahpla"
+func Reverse(word string) (reversed string) {
+	reversed = ""
+	for _, v := range word {
+		reversed = string(v) + reversed
+	}
+	return
 }
