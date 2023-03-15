15da901b07f9be3d1ceb6434d187be90bd01deda
xDouglas90 <odouglas.dev@gmail.com>
2023-03-15 13:46:53 -0300

    feat(module-1): implements Sum functionality :sparkles:

--- module01/sum.go
+++ module01/sum.go
@@ -2,6 +2,10 @@ package module01
 
 // Sum will sum up all of the numbers passed
 // in and return the result
-func Sum(numbers []int) int {
-	return 0
+func Sum(numbers []int) (total int) {
+	total = 0
+	for _, v := range numbers {
+		total += v
+	}
+	return
 }
